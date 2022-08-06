2b9f74efac7a6b2bcdc5e418e51a44197b864cb8
Belal Fayez <37405227+bilalfayez@users.noreply.github.com>
2022-08-06 05:02:00 +0200

    Version Test 2.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+    val version = "This version 2"
+    //I've updated this file to V2.
+    println(version)
+}
